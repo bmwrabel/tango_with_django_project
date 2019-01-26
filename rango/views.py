b5907120d264bfb6828670abdfb6ec923b81acd1
“Borys <2206447w@student.gla.ac.uk>
2019-01-26 06:29:28 +0000

    Chapter 4

--- rango/views.py
+++ rango/views.py
@@ -6,9 +6,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    html= 'Rango says hey there partner!<br/>' + '<a href="/rango/about">About</a>'
-    return HttpResponse(html)
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    html= 'Rango says here is the about page.<br/>' + '<a href="/rango/">Index</a>'
-    return HttpResponse(html)
+    context_dict = {'boldmessage': 'This tutorial has been put together by Borys Wrabel.'}
+    return render(request, 'rango/about.html', context=context_dict)
