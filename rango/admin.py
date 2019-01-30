8b412e5473e76f0634fbbfd1a1badecc0f0c4c58
“bmwrabel” <boris.wrabel@googlemail.com>
2019-01-30 04:03:20 +0000

    Chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, PageAdmin
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 class Category(models.Model):
@@ -23,3 +24,6 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
