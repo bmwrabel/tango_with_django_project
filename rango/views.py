fdea81453a54ca26151f735dc0157244d7049207
“bmwrabel” <boris.wrabel@googlemail.com>
2019-01-31 02:49:40 +0000

    Chapter 6 half

--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
-def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request, 'rango/index.html', context=context_dict)
+# Import the Category model
+from rango.models import Category
 
+def index(request):
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
+    return render(request, 'rango/index.html', context_dict)
+    
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Borys Wrabel.'}
+    
     return render(request, 'rango/about.html', context=context_dict)
