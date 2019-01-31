55d0a1bea3e8a9dc471db43ba254ef4d73c16d33
“bmwrabel” <boris.wrabel@googlemail.com>
2019-01-31 15:24:29 +0000

    Chapter 6 completed

--- rango/admin.py
+++ rango/admin.py
@@ -2,8 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from rango.models import Category, Page, PageAdmin
+from rango.models import Category, Page
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
 
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
--- rango/models.py
+++ rango/models.py
@@ -2,13 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib import admin
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -25,5 +30,3 @@ class Page(models.Model):
     def __str__(self):
         return self.title
 
-class PageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'url')
